30794db76d17848a4fb8c3b5f9e2bd5f0f0c2077
William Spada <williamrspada02@gmail.com>
2022-05-05 19:15:27 -0300

    aula 02 - aceleracao maxima

---
+++ src/components/WidgetForm/index.tsx
@@ -0,0 +1,76 @@
+    import { useState } from "react";
+    import { CloseButton } from "../CloseButton";
+    import { FeedbackTypeStep } from "./Steps/FeedbackTypeStep";
+
+    import bugImageUrl from "../../assets/bug.svg";
+    import ideaImageUrl from "../../assets/idea.svg";
+    import thoughtImageUrl from "../../assets/thought.svg";
+    import { FeedbackContentStep } from "./Steps/FeedbackContentStep";
+    import { FeedbackSucessStep } from "./Steps/FeedbackSucessStep";
+
+    export const feedbackTypes = {
+        BUG: {
+            title: "Problema",
+            image: {
+                source: bugImageUrl,
+                alt: 'Imagem de um inseto'
+            }
+        },
+        IDEA:{
+            title: "Ideia",
+            image: {
+                source: ideaImageUrl,
+                alt: 'Imagem de uma lampada'
+            }
+        },
+        OTHER:{
+            title: "Outro",
+            image: {
+                source: thoughtImageUrl,
+                alt: 'Imagem de um balao de pensamento'
+            },
+        },
+    };
+
+    export type FeedbackType = keyof typeof feedbackTypes;
+
+    export function WidgetForm(){
+
+        const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null);
+        const [feedbackSent, setFeedbackSent] = useState(false);
+
+        function handleRestartRequested(){
+            setFeedbackSent(false);
+            setFeedbackType(null);
+        }
+
+        return(
+            <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
+                { feedbackSent ? (
+                    <FeedbackSucessStep
+                    onFeedbackRestartRequested={handleRestartRequested}
+                    />
+                ) : 
+                <>
+                    {!feedbackType ? 
+                        <FeedbackTypeStep 
+                        onFeedbackTypeChanged={setFeedbackType}
+                        />
+                    : 
+                    (
+                    <FeedbackContentStep
+                    feedbackType={feedbackType}
+                    onFeedbackRestartRequested={handleRestartRequested}
+                    onFeedbackSent={() => setFeedbackSent(true)}
+                    />
+                    )}
+                </>
+                }
+
+
+                <footer className="text-xs text-neutral-400">
+                    Feito com ♥ pela <a className="underline underline-offset-2" href="https://rocketseat.com.br">Rocketseat</a> 
+                </footer>
+            </div>
+        );
+    }
